153543f41311c2fb2be2b57c11ffec8c932f7532
Nick Evans <2616208+nickevansuk@users.noreply.github.com>
2018-10-31 22:45:13 +0000

    Create query-1.js

---
+++ example-responses/event-series/query-1.js
@@ -0,0 +1,181 @@
+{
+  "type": "EventSeries",
+  "activity": [
+    {
+      "type": "Concept",
+      "id": "https://openactive.io/activity-list#fe2c9859-aa69-43c5-bcc3-ba5a6c7d9815",
+      "prefLabel": "Clubbercise Kids"
+    }
+  ],
+  "ageRange": {
+    "type": "QuantitativeValue",
+    "minValue": 8,
+    "maxValue": 22
+  },
+  "genderRestriction": "oa:MaleOnly",
+  "isCoached": true,
+  "imin:level": [
+    {
+      "type": "imin:BeginnerLevel",
+      "name": "Beginner"
+    },
+    {
+      "type": "imin:AdvancedLevel",
+      "name": "Advanced"
+    }
+  ],
+  "programme": {
+    "type": "Brand",
+    "name": "This Girl Can",
+    "url": "https://myrtice.com",
+    "description": "Deserunt minus perspiciatis itaque. Debitis voluptatem voluptates maxime qui a. Illo fugit rem quidem modi natus. Et et aliquid beatae non nobis suscipit. Officiis id repellendus amet fugiat ab quia non rem. Dolor cum ex ut officiis ex.\n \rSapiente unde hic et numquam. Autem ut qui. Et cupiditate rerum beatae harum ut et et. Laboriosam tempora veniam ut voluptas vitae qui hic excepturi.\n \rQui delectus ipsam delectus pariatur autem deserunt sint velit ea. Ut architecto cum corrupti qui eum aut. Enim quisquam dolor. Iusto et aut officiis facere quaerat quas ab est eius. Dolor iusto sunt. Nostrum adipisci excepturi dolores ut ut ad in.\n \rAccusamus minus sed enim voluptatem consectetur possimus. Saepe delectus omnis numquam et. Libero sint enim aut eveniet similique unde voluptatem qui. Alias doloribus laboriosam optio repellat asperiores dolore qui libero ea. Aut aut enim dolores molestias et repellat accusantium minima.",
+    "logo": {
+      "type": "ImageObject",
+      "url": "https://s3.amazonaws.com/uifaces/faces/twitter/vytautas_a/128.jpg"
+    }
+  },
+  "description": "Nesciunt consequatur expedita aliquam nobis quasi ipsum quam explicabo possimus. Tempora voluptatem doloribus rerum. Id error et nisi autem excepturi similique. Enim sint autem aspernatur id quisquam et molestias accusantium.\n \rDebitis non perspiciatis sunt in. Vitae voluptatum consequatur. Magnam doloribus nobis repellendus doloremque voluptatem voluptatibus consequuntur pariatur. Fugit eum qui sit eligendi voluptatem voluptatum perferendis. Veritatis provident beatae vitae maxime accusantium eos non.\n \rQui alias est doloremque est optio tempora veniam voluptatem. Aut qui laudantium quo omnis blanditiis ipsam velit adipisci aliquid. Ab nemo reprehenderit voluptatem reiciendis et id. Harum et voluptas quia doloremque sequi sint et reprehenderit consequatur.\n \rSunt necessitatibus vel quia. Cumque qui fugit in tempora. Ex eos cupiditate impedit culpa maxime. Corporis sed qui placeat est labore quis dignissimos. Nostrum quis magnam qui qui. Ab qui vitae dicta possimus et totam dolorum.",
+  "beta:estimatedDuration": {
+    "type": "QuantitativeValue",
+    "value": 30,
+    "unitCode": "MIN"
+  },
+  "image": [
+    {
+      "type": "ImageObject",
+      "url": "https://picsum.photos/1024/768?image=231"
+    },
+    {
+      "type": "ImageObject",
+      "url": "https://picsum.photos/1024/768?image=171"
+    },
+    {
+      "type": "ImageObject",
+      "url": "https://picsum.photos/1024/768?image=834"
+    },
+    {
+      "type": "ImageObject",
+      "url": "https://picsum.photos/1024/768?image=5"
+    },
+    {
+      "type": "ImageObject",
+      "url": "https://picsum.photos/1024/768?image=101"
+    }
+  ],
+  "isAccessibleForFree": false,
+  "name": "GOLDEN: Virtual BODYPUMP",
+  "imin:aggregateOffer": {
+    "publicAdult": {
+      "type": "AggregateOffer",
+      "price": 17.04,
+      "imin:membershipRequired": false,
+      "priceCurrency": "GBP"
+    },
+    "publicJunior": {
+      "type": "AggregateOffer",
+      "price": 25.53,
+      "imin:membershipRequired": false,
+      "priceCurrency": "GBP"
+    }
+  },
+  "organizer": {
+    "type": "Person",
+    "name": "Gianni Becker",
+    "givenName": "Gianni",
+    "gender": "schema:Female",
+    "image": {
+      "type": "ImageObject",
+      "url": "https://s3.amazonaws.com/uifaces/faces/twitter/uxpiper/128.jpg"
+    }
+  },
+  "id": "https://search-sandbox.imin.co/events-api/v2/event-series/loremfitsum-1539623400",
+  "imin:dataSource": {
+    "type": "WebAPI",
+    "identifier": "loremfitsum"
+  },
+  "subEvent": [
+    {
+      "type": "SessionSeries",
+      "location": {
+        "type": "Place",
+        "url": "http://donnie.com/centres/wiza-manors-leisure-centre",
+        "name": "Wiza Manors Leisure Centre",
+        "description": "Rerum reprehenderit nemo hic libero omnis voluptatem architecto laudantium commodi. Nisi ut sit vel ullam id. Tenetur iste non ad quia sit dolores dolorum ipsam nulla.\n \rAut mollitia sunt ut et exercitationem temporibus et. Culpa neque dolor sapiente corrupti magni ea. Deserunt dolorem et dolore saepe distinctio distinctio.\n \rDistinctio sint aut molestias sit provident non quibusdam distinctio. Quia perferendis rem quia neque quia voluptatem. Soluta iure hic. Esse dolorum blanditiis molestiae rem inventore occaecati iste. Aperiam qui et aliquam odit pariatur sint. Ea repudiandae veritatis amet odit quod dolor voluptatem.\n \rVelit consectetur voluptas esse omnis quod et qui. Et reiciendis aut fugit dolor ut voluptatem. Aperiam ad velit cumque ullam. Porro quidem maiores commodi ut ut. Minus minus repellendus consequatur voluptatibus et ab repellendus saepe rerum. Est quae consequatur et.",
+        "address": {
+          "type": "PostalAddress",
+          "addressCountry": "GB",
+          "imin:fullAddress": "Wiza Manors Leisure Centre, 5401 Doyle Forks, Oxford, Oxfordshire, OX4 7UF"
+        },
+        "telephone": "020 9445 4861",
+        "geo": {
+          "type": "GeoCoordinates",
+          "latitude": 51.72093285,
+          "longitude": -1.18749529,
+          "imin:distanceFromGeoQueryCenter": {
+            "type": "QuantitativeValue",
+            "value": 77.153,
+            "unitCode": "KMT"
+          }
+        },
+        "image": [
+          {
+            "type": "ImageObject",
+            "url": "https://picsum.photos/1024/768?image=1071"
+          }
+        ],
+        "amenityFeature": [
+          {
+            "type": "Showers",
+            "value": true,
+            "name": "Showers"
+          },
+          {
+            "type": "Lockers",
+            "value": true,
+            "name": "Lockers"
+          },
+          {
+            "type": "Creche",
+            "value": true,
+            "name": "Creche"
+          }
+        ]
+      },
+      "eventSchedule": [
+        {
+          "type": "PartialSchedule",
+          "repeatFrequency": "P1W",
+          "byDay": [
+            "schema:Wednesday",
+            "schema:Tuesday",
+            "schema:Saturday",
+            "schema:Monday",
+            "schema:Thursday",
+            "schema:Friday",
+            "schema:Sunday"
+          ],
+          "startTime": "04:30",
+          "endTime": "05:00",
+          "duration": "PT30M"
+        }
+      ],
+      "subEvent": [
+        {
+          "type": "ScheduledSession",
+          "startDate": "2018-10-30T04:30:00Z",
+          "endDate": "2018-10-30T05:00:00Z"
+        },
+        {
+          "type": "ScheduledSession",
+          "startDate": "2018-10-30T09:15:00Z",
+          "endDate": "2018-10-30T09:45:00Z"
+        },
+        {
+          "type": "ScheduledSession",
+          "startDate": "2018-10-21T09:15:00Z",
+          "endDate": "2018-10-21T09:45:00Z"
+        }
+      ]
+    }
+  ]
+}
